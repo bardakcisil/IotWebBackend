111940289bb27da161a66f29d3bbadd0c70402a1
Işıl BARDAKCI <bardakci.isil@hotmail.com>
2022-08-31 09:14:07 +0300

    Add project files.

---
+++ IotWeb.Models/SensorData.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IotWeb.Models
+{
+    public class SensorData
+    {
+        [BsonId]
+        public ObjectId _Id { get; set; }
+        public int Humidity { get; set; }
+        public int Temprature { get; set; }
+    }
+}
